b631f398c3ddb2b9133982684f3bea88ca97e0a5
Joshua Whitley <maximus5684@gmail.com>
2017-10-08 02:27:21 +0000

    Adding cxxopts and creating a basic window for testing.

--- include/dash_pi.hpp
+++ include/dash_pi.hpp
@@ -1,6 +1,10 @@
 #ifndef DASH_PI_HPP
 #define DASH_PI_HPP
 
+#include <vector>
+#include <iostream>
+
+#include <cxxopts.hpp>
 #include <SFML/Graphics.hpp>
 
 namespace DashPi
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,32 @@
 #include <dash_pi.hpp>
 
+using namespace sf;
+using namespace DashPi;
+
 int main (int argc, char * argv[])
 {
+  //Get all of the available full-screen modes.
+  std::vector<VideoMode> fs_vms = VideoMode::getFullscreenModes();
+  //Select the first (best) one.
+  VideoMode vm = fs_vms[0];
+  std::cout << "Is valid: " << vm.isValid() << " Size: " << vm.width << " x " << vm.height << "." << std::endl;
+  
+  RenderWindow window(vm, "Dash Pi", sf::Style::Fullscreen);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+      if (event.type == Event::Closed)
+        window.close();
+    }
+
+    window.clear(Color::Blue);
+
+    window.display();
+  }
+
+  return 0;
 }
