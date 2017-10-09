4b849d2c9edd4b0f0d720652ec68c12ca27b9813
Josh Whitley <maximus5684@gmail.com>
2017-10-09 02:00:52 -0500

    Cleaning up install_prereqs.sh.

--- install_scripts/install_prereqs.sh
+++ install_scripts/install_prereqs.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 sudo apt update
+
+#Prereqs for sdbusplus
+sudo apt install autoconf libtool libsystemd-dev autoconf-archive python-yaml
+pip install inflection
+
+#Prereqs for sfml-pi
 sudo apt install python-bluez cmake libflac-dev libogg-dev libvorbis-dev libopenal-dev libjpeg8-dev libfreetype6-dev libudev-dev libraspberrypi-dev
 
-rm -rf depends
+sudo rm -rf depends
 mkdir depends && cd depends
 
+echo ""
 echo "Installing sdbusplus..."
+echo ""
 sleep 2
 
 git clone https://github.com/openbmc/sdbusplus
 cd sdbusplus
+./bootstrap.sh
+./configure
+make
+sudo make install
 cd ..
 
+echo ""
 echo "Installing sfml-pi..."
+echo ""
 sleep 2
 
 git clone https://github.com/mickelson/sfml-pi
@@ -26,4 +40,5 @@ cd ..
 
 cd ..
 
+echo ""
 echo "All done!"
